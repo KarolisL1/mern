179a489677f4b3d1895ee8f372ec91ba8cbdf9eb
karolisliepa <Karolis.Liepa@gmail.com>
2022-03-09 17:34:11 -0500

    adding {}

--- react/putting_it_together/src/App.js
+++ react/putting_it_together/src/App.js
@@ -5,8 +5,8 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard fname="Jane" lname="Doe" age="45" color="Black"/>
-      <PersonCard fname="John" lname="Smith" age="88" color="Brown"/>
+      <PersonCard fname={"Jane"} lname="Doe" age={45} color="Black"/>
+      <PersonCard fname={"John"} lname="Smith" age={88} color="Brown"/>
     </div>
   );
 }
