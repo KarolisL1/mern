9983e0e3d38db4d79abbfc9ce3865dec993ea518
karolisliepa <Karolis.Liepa@gmail.com>
2022-03-10 18:19:27 -0500

    more forms

---
+++ react/more_forms/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UsersForm from './components/UsersForm';
+
+function App() {
+  return (
+    <div className="App">
+      <UsersForm />
+    </div>
+  );
+}
+
+export default App;
